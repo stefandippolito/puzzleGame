1f15bc991d476415a6894879040d776745f26494
stefandippolito <98289422+stefandippolito@users.noreply.github.com>
2022-05-08 20:18:28 -0400

    ReCoded the colour switch and added more colours
    Added lightblue, pink, white, green, red and yellow

--- Puzzle Game/Assets/Scripts/changeColour.cs
+++ Puzzle Game/Assets/Scripts/changeColour.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class changeColour : MonoBehaviour
 {
-    private Color myColor;
+    public Color myColor;
+    
     private Renderer myRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        myRenderer = gameObject.GetComponent<Renderer>();
+        
     }
 
     // Update is called once per frame
@@ -17,16 +18,54 @@ public class changeColour : MonoBehaviour
     {
         
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("hit");
-        if(other.gameObject.tag == "red")
+        myRenderer = gameObject.GetComponent<Renderer>();
+        if (other.gameObject.tag == "red")
         {
+            Debug.Log("hit");
+
             // new Color(red,green,blue,a)
-            myColor = new Color(1, 0, 0, 1);
-            myRenderer.material.color = myColor;
-            Destroy(other);
+            
+            myRenderer.material.color = new Color(1, 0, 0, 1);
+            Destroy(other.gameObject);
+
         }
+        
+        else if (other.gameObject.tag == "green")
+        {
+            
+            myRenderer.material.color = new Color(0, 1, 0, 1);
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag == "yellow")
+        {
+            myRenderer.material.color = new Color(1, 1, 0, 1);
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag == "pink")
+        {
+            myRenderer.material.color = new Color(1, 0, 1, 1);
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag == "lightBlue")
+        {
+            myRenderer.material.color = new Color(0, 1, 1, 1);
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag == "white")
+        {
+            myRenderer.material.color = new Color(1, 1, 1, 1);
+            Destroy(other.gameObject);
+        }
+
+
+
+
     }
 }
