b7d5ddd126a00c3080ec83dd15b432fe56d01553
stefandippolito <98289422+stefandippolito@users.noreply.github.com>
2022-05-09 01:47:35 -0400

    Created the rest of the prototype enemies

--- Puzzle Game/Assets/Scripts/enemyInteract.cs
+++ Puzzle Game/Assets/Scripts/enemyInteract.cs
@@ -12,14 +12,14 @@ public class enemyInteract : MonoBehaviour
         // if colour of front rectange is the same as the enemey rectangle then destroy enemy rectangle and proceed with the level
         if ((other.gameObject.tag == "redEnemy" && gameObject.tag == "red") || (other.gameObject.tag == "greenEnemy" && gameObject.tag == "green") || 
             (other.gameObject.tag == "yellowEnemy" && gameObject.tag == "yellow") || (other.gameObject.tag == "pinkEnemy" && gameObject.tag == "pink") ||
-            (other.gameObject.tag == "lightblueEnemy" && gameObject.tag == "lightblue") || (other.gameObject.tag == "whiteEnemy" && gameObject.tag == "white"))
+            (other.gameObject.tag == "lightBlueEnemy" && gameObject.tag == "lightBlue") || (other.gameObject.tag == "whiteEnemy" && gameObject.tag == "white"))
         {
             Destroy(other.gameObject);
 
         }
         //if front rectangle colides with an enemy and it s not the same colour then restart level
         else if (other.gameObject.tag == "redEnemy" || other.gameObject.tag == "greenEnemy" || other.gameObject.tag == "yellowEnemy" ||
-            other.gameObject.tag == "pinkEnemy" || other.gameObject.tag == "lightblueEnemy" || other.gameObject.tag == "whiteEnemy")
+            other.gameObject.tag == "pinkEnemy" || other.gameObject.tag == "lightBlueEnemy" || other.gameObject.tag == "whiteEnemy")
         {
             Application.LoadLevel(Application.loadedLevel);
         }
