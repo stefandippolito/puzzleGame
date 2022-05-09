440cb125ba64ec99e1201e2569c0d0b9f1339111
stefandippolito <98289422+stefandippolito@users.noreply.github.com>
2022-05-09 17:26:56 -0400

    Added Pause Menu
    Added pause Menu and imported Text Mesh Pro

--- Puzzle Game/Assets/Scripts/cameraMovement.cs
+++ Puzzle Game/Assets/Scripts/cameraMovement.cs
@@ -6,13 +6,7 @@ public class cameraMovement : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, player.position.z + offset.z);
